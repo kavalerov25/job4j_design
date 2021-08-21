c46c3bfdc0b92b813f756d0417097048f728f0c2
kavalerov25 <paradise_777@mail.ru>
2021-08-21 20:41:27 +0300

    0. Что такое деревья? [#4956 #192734]

--- src/test/java/ru/job4j/tree/SimpleTreeTest.java
+++ src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -3,18 +3,44 @@ package ru.job4j.tree;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 
-public class SimpleTreeTest {
 
+public class SimpleTreeTest {
     @Test
-    public void whenFind() {
+    public void when6ElFindLastThen6() {
         Tree<Integer> tree = new SimpleTree<>(1);
         tree.add(1, 2);
         tree.add(1, 3);
         tree.add(1, 4);
         tree.add(4, 5);
         tree.add(5, 6);
-        assertThat(tree.findBy(4).isPresent(), is(true));
+        assertThat(
+                tree.findBy(6).isPresent(),
+                is(true)
+        );
     }
+
+    @Test
+    public void when6ElFindNotExitThenOptionEmpty() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        assertThat(
+                tree.findBy(7).isPresent(),
+                is(false)
+        );
+    }
+
+    @Test
+    public void whenChildExistOnLeafThenNotAdd() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertFalse(tree.add(2, 6));
+    }
+
 }
