04347acb9591319218e8f58438b5944831eabed8
kavalerov25 <paradise_777@mail.ru>
2021-09-01 21:21:43 +0300

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,53 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Config {
+
+    private final String path;
+    private Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
+            in.lines().forEach(s -> {
+                if (!s.startsWith("#") && s.contains("=") && !s.startsWith("//")) {
+                    String[] points = s.split("=");
+                    if (points.length <= 2) {
+                        values.put(points[0], points[1]);
+                    }
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        Config config = new Config("app.properties");
+        System.out.println(config);
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+import org.hamcrest.Matchers;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithComment() {
+        String path = "./data/pair_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("first"), is("Arsentev"));
+        assertThat(config.value("second"), is("Korobeynikov "));
+    }
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("first"), is("Arsentev "));
+        assertThat(config.value("second"), is(Matchers.nullValue()));
+    }
+}
+
