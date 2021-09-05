4d678335b641a574377c69ca3a4da66e495ebce9
kavalerov25 <paradise_777@mail.ru>
2021-09-05 19:36:08 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -4,7 +4,10 @@ import java.io.File;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("c:\\projects\\job4j_design");
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        File file = new File(args[0]);
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,9 +9,16 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith("xml")).forEach(System.out::println);
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Root folder or extension of file is null. Usage java -jar dir.jar ROOT_FOLDER EXTENSION_FILE.");
+        }
+        Path start = Paths.get(args[0]);
+        System.out.println(search(start, p -> p
+                .toFile()
+                .getName()
+                .endsWith(args[1])));
     }
+
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
