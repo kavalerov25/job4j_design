8c90d41010b90367cb8e53b5ca755e9b78c9975c
kavalerov25 <paradise_777@mail.ru>
2021-09-18 21:00:50 +0300

    1. Что такое Сериализация? [#313163]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,26 +1,35 @@
 package ru.job4j.io;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
+            System.out.println("Сервер запущен!");
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write("Hello, dear friend.".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
-                        if (str.contains("Bye")) {
-                            System.out.println("the server is stopped.");
+                        if (str.contains("Exit")) {
+                            System.out.println("server is stopped.");
                             server.close();
+                        } else if (str.contains("Hello")) {
+                            out.write("Hello".getBytes());
+                        } else if (str.contains("msg=")) {
+                            out.write("What".getBytes());
                         }
+                        out.flush();
                     }
-                    out.flush();
                 }
             }
         }
