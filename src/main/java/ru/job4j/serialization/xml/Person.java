11d27d7bc9d31a6957b4785a5941aac0046b1b85
kavalerov25 <paradise_777@mail.ru>
2021-09-20 20:03:47 +0300

    3. Формат XML [#313165]

---
+++ src/main/java/ru/job4j/serialization/xml/Person.java
@@ -0,0 +1,33 @@
+package ru.job4j.serialization.xml;
+
+import java.util.Arrays;
+
+public class Person {
+
+    private final boolean sex;
+    private final int age;
+    private final Contact contact;
+    private final String[] statuses;
+
+    public Person(boolean sex, int age, Contact contact, String... statuses) {
+        this.sex = sex;
+        this.age = age;
+        this.contact = contact;
+        this.statuses = statuses;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{"
+               + "sex=" + sex
+               + ", age=" + age
+               + ", contact=" + contact
+               + ", statuses=" + Arrays.toString(statuses)
+               + '}';
+    }
+
+    public static void main(String[] args) {
+        final Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
+        System.out.println(person);
+    }
+}
