8c80ba83263731c536bc53ef47029c76ffafb081
kavalerov25 <paradise_777@mail.ru>
2021-10-01 22:12:40 +0300

    0. Что такое база данных. [#6860]

---
+++ data/books.sql
@@ -0,0 +1,6 @@
+insert into books(author,name,read) values('Оруэлл','1984','true');
+select * from books;
+update books set read= 'false';
+select * from books;
+delete from books;
+select * from books;
