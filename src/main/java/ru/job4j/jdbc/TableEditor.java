6877d0c019887b72b4b27eb6c5be0088cd27821a
kavalerov25 <paradise_777@mail.ru>
2021-11-07 23:25:42 +0300

    0.1. Statement [#379306]

---
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -0,0 +1,80 @@
+package ru.job4j.jdbc;
+
+import java.sql.*;
+import java.util.Properties;
+import java.util.StringJoiner;
+
+public class TableEditor implements AutoCloseable {
+
+    private Connection connection;
+
+    private Properties properties;
+
+    public TableEditor(Properties properties) throws SQLException, ClassNotFoundException {
+        this.properties = properties;
+        initConnection();
+    }
+
+    private void initConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+        String url = properties.getProperty("url");
+        String login = properties.getProperty("login");
+        String password = properties.getProperty("password");
+        connection = DriverManager.getConnection(url, login, password);
+    }
+
+    private void action(String sql) {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void createTable(String tableName) {
+        action(String.format("create table if not exists %s();", tableName));
+    }
+
+    public void dropTable(String tableName) {
+        action(String.format("drop table if exists %s;", tableName));
+    }
+
+    public void addColumn(String tableName, String columnName, String type) {
+        action(String.format("alter table %s add column %s %s;", tableName, columnName, type));
+    }
+
+    public void dropColumn(String tableName, String columnName) {
+        action(String.format("alter table %s drop column %s;", tableName, columnName));
+    }
+
+    public void renameColumn(String tableName, String columnName, String newColumnName) {
+        action(String.format("alter table %s rename column %s to %s;", tableName, columnName, newColumnName));
+    }
+
+
+    public static String getTableScheme(Connection connection, String tableName) throws Exception {
+        var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
+        var header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
+        var buffer = new StringJoiner(rowSeparator, rowSeparator, rowSeparator);
+        buffer.add(header);
+        try (var statement = connection.createStatement()) {
+            var selection = statement.executeQuery(String.format(
+                    "select * from %s limit 1", tableName
+            ));
+            var metaData = selection.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                buffer.add(String.format("%-15s|%-15s%n",
+                        metaData.getColumnName(i), metaData.getColumnTypeName(i))
+                );
+            }
+        }
+        return buffer.toString();
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+}
