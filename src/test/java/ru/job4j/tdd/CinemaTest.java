727a31d7703ef2bb534e441f56ea512986889a18
kavalerov25 <paradise_777@mail.ru>
2022-05-15 17:19:52 +0300

    2. Что такое TDD? [#4918 #300999] добавить тесты на ситуации 1) невалидное место 2)невалидная дата

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -1,15 +1,15 @@
 package ru.job4j.tdd;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.Matchers.is;
-
-
 import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class CinemaTest {
 
@@ -41,15 +41,35 @@ public class CinemaTest {
         assertEquals(cinema.find(session -> true).get(0), new Session3D());
     }
 
-        @Ignore
-        @Test(expected = IllegalArgumentException.class)
-        public void whenBuyBusyPlace() {
-            Account first = new AccountCinema();
-            Account sec = new AccountCinema();
-            Cinema cinema = new Cinema3D();
-            Calendar date = Calendar.getInstance();
-            date.set(2020, 10, 10, 23, 00);
-            Ticket firstTicket = cinema.buy(first, 1, 1, date);
-            Ticket secondTicket = cinema.buy(first, 1, 1, date);
-        }
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenBuyBusyPlace() {
+        Account first = new AccountCinema();
+        Account sec = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket firstTicket = cinema.buy(first, 1, 1, date);
+        Ticket secondTicket = cinema.buy(first, 1, 1, date);
     }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenBuyNoValidDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 13, 30, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 10, date);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNoValidPlace() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 12, 30, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 100, date);
+    }
+}
