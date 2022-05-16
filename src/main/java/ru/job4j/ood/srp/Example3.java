207b810a2d968fe3b7009d40458c789ddb4b1d3f
kavalerov25 <paradise_777@mail.ru>
2022-05-16 10:22:05 +0300

    0. Принцип единственной ответственности [#4913]

--- src/main/java/ru/job4j/ood/srp/Example3.java
+++ src/main/java/ru/job4j/ood/srp/Example3.java
@@ -2,11 +2,11 @@ package ru.job4j.ood.srp;
 
 public interface Example3 {
 
-     void measureTempera();  /* измеряем температуру*/
+     void measureTempera();
 
-     void controlTime();  /* засекаем время */
+     void controlTime();
 }
 
-/* Допустим имплементируем интерфейс в класс Секундомер. В этом классе не нужен метод measureTempera
+/** Допустим имплементируем интерфейс в класс Секундомер. В этом классе не нужен метод measureTempera
 *  нужно разделить функционал
 * */
--- src/main/java/ru/job4j/ood/srp/Service.java
+++ src/main/java/ru/job4j/ood/srp/Service.java
@@ -4,16 +4,16 @@ import java.awt.*;
 
 public class Service {
     public static void saveImage(Image img) {
-        /* */
+        /** */
     }
 
     public static void printImage(Image img) {
-        /* */
+        /** */
     }
 
     public static void sendEmailMessage(String address, String subject, String text) {
-        /* */
+        /** */
     }
 }
 
-/* Данный класс нарушает принцип SRP, так как он содержит в себе разный функционал*/
+/** Данный класс нарушает принцип SRP, так как он содержит в себе разный функционал*/
