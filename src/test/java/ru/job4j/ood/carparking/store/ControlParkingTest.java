b3c7e8d421aef5e5ff0d58b6c1e32f484b3a6ab0
kavalerov25 <paradise_777@mail.ru>
2022-06-09 20:19:05 +0300

    2. Парковка машин [#853 #306328]

--- src/test/java/ru/job4j/ood/carparking/store/ControlParkingTest.java
+++ src/test/java/ru/job4j/ood/carparking/store/ControlParkingTest.java
@@ -5,21 +5,24 @@ import org.junit.Test;
 import ru.job4j.ood.carparking.model.Car;
 import ru.job4j.ood.carparking.model.Truck;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class ControlParkingTest {
     @Test
     @Ignore
     public void whenTwoPassengerAndOneTruckPlacesThanTwoTrucks() {
-        var parking = new ControlParking (2, 1);
+        var parking = new ControlParking(2, 1);
         var truck = new Truck(2);
         assertTrue(parking.park(truck));
         assertTrue(parking.park(truck));
         assertFalse(parking.park(truck));
     }
-    @Test @Ignore
+
+    @Test
+    @Ignore
     public void whenTwoPassengerAndOneTruckPlacesThanOneCarAndTwoTrucks() {
-        var parking = new ControlParking (2, 1);
+        var parking = new ControlParking(2, 1);
         var car = new Car(1);
         var truck = new Truck(2);
         assertTrue(parking.park(car));
@@ -29,9 +32,10 @@ public class ControlParkingTest {
         assertFalse(parking.park(car));
     }
 
-    @Test @Ignore
+    @Test
+    @Ignore
     public void whenTwoPassengerAndNoTruckPlacesThanOneTruckAndNoCar() {
-        var parking = new ControlParking (2, 0);
+        var parking = new ControlParking(2, 0);
         var car = new Car(1);
         var truck = new Truck(2);
         assertTrue(parking.park(truck));
@@ -39,9 +43,10 @@ public class ControlParkingTest {
         assertFalse(parking.park(truck));
     }
 
-    @Test @Ignore
+    @Test
+    @Ignore
     public void whenNoPassengerAndOneTruckPlacesThanNoCar() {
-        var parking = new ControlParking (0, 1);
+        var parking = new ControlParking(0, 1);
         var car = new Car(1);
         assertFalse(parking.park(car));
     }
