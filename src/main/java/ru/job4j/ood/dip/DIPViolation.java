985e00d9c3783bcaf669997e582536624d141a34
kavalerov25 <paradise_777@mail.ru>
2022-06-19 11:01:44 +0300

    0. Принцип инверсии зависимостей [#4917]

---
+++ src/main/java/ru/job4j/ood/dip/DIPViolation.java
@@ -0,0 +1,36 @@
+package ru.job4j.ood.dip;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+/**
+1. Нарушение DIP - в поле класса прямая зависимость от конкретной реализации.
+ */
+public class DIPViolation {
+    private HashSet<Integer> hashSet = new HashSet<>();
+
+}
+
+/**
+2. Нарушение DIP - возвращаемое значение метода имеет конкретную реализацию.
+ */
+class DIPViolation2 {
+
+    public ArrayList<String> list() {
+        //...
+        return new ArrayList<>();
+    }
+}
+
+/**
+3. Нарушение DIP - аргумент конструктора имеет конкретную реализацию.
+ */
+class DIPViolation3 {
+    private Map<Integer, Integer> map;
+
+    public DIPViolation3(HashMap<Integer, Integer> map) {
+        this.map = map;
+    }
+}
