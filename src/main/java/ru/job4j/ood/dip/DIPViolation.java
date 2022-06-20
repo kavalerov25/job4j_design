31ed39ddfaca8f7f4066e2018e85a3e8b987cfe2
kavalerov25 <paradise_777@mail.ru>
2022-06-21 00:38:38 +0300

    checkstyle

--- src/main/java/ru/job4j/ood/dip/DIPViolation.java
+++ src/main/java/ru/job4j/ood/dip/DIPViolation.java
@@ -5,19 +5,28 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 
+/**
+ * 1. Нарушение DIP - в поле класса прямая зависимость от конкретной реализации.
+ */
 public class DIPViolation {
     private HashSet<Integer> hashSet = new HashSet<>();
 
 }
 
+/**
+ * 2. Нарушение DIP - возвращаемое значение метода имеет конкретную реализацию.
+ */
 class DIPViolation2 {
 
     public ArrayList<String> list() {
-        //...
+
         return new ArrayList<>();
     }
 }
 
+/**
+ * 3. Нарушение DIP - аргумент конструктора имеет конкретную реализацию.
+ */
 class DIPViolation3 {
     private Map<Integer, Integer> map;
 
